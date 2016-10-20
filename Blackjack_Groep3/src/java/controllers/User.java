554ee726475d04f9c5f1d32e1ac7ecf88857913b
Xander <Xander@WKS005>
2016-10-20 09:27:14 +0200

    Package Controllers and Class User added.

---
+++ Blackjack_Groep3/src/java/controllers/User.java
@@ -0,0 +1,15 @@
+/*
+ * Xander is e Legend
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author Xander
+ */
+public class User {
+    
+}
